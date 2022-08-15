d3fde8e62675c8530cce701ff338411657751b90
Axel Broeglin <axelbroeglin@gmail.com>
2022-08-15 21:00:48 +0100

    Fixed content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './public/index.html',
-    './public/script.js'
+    './public/*.{html,js}',
+    './public/styles.css'
   ],
   theme: {
     extend: {},
