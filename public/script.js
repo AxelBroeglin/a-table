0e92aa9d1d48961ef3bcb15784903b9557adb257
Axel Broeglin <axelbroeglin@gmail.com>
2022-08-15 21:53:40 +0100

    Started working on fetch

--- public/script.js
+++ public/script.js
@@ -1,3 +1,17 @@
 const foodSearch = document.getElementById('food-search');
 const btn = document.getElementById('btn');
 const cardsDisplay = document.getElementById('cards-display');
+
+const options = {
+	method: 'GET',
+	headers: {
+        //to be found in doc
+		'xxx': 'SIGN-UP-FOR-KEY',
+		'xxx': 'edamam-recipe-search.p.rapidapi.com'
+	}
+};
+
+fetch('https://api.edamam.com/api/recipes/v2/', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
