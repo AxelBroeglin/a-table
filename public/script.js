eea69de0924b99102d915f69b52528d88242f58d
Axel Broeglin <axelbroeglin@gmail.com>
2022-08-16 22:03:11 +0100

    Work on form

--- public/script.js
+++ public/script.js
@@ -1,10 +1,18 @@
-const foodSearch = document.getElementById('food-search');
+const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('btn');
 const cardsDisplay = document.getElementById('cards-display');
 const app_id = '1ab55c64';
-const api_key = 'c86872049aff2debad57830e690d77c8'
+const api_key = 'c86872049aff2debad57830e690d77c8';
+let foodSearch = '';
 
-fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=${app_id}&app_key=${api_key}`)
+searchButton.addEventListener('click', ()=>{
+	event.preventDefault();
+	foodSearch = searchInput.value;
+	console.log(foodSearch);
+	return foodSearch;
+})
+
+fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=soup&app_id=${app_id}&app_key=${api_key}`)
 	.then(response => response.json())
 	.then(response => useApiResponse(response))
 	.catch(err => console.error(err));
