d77a41c9bc47e4770e7b84ab849eb80f3f808f39
Axel Broeglin <axelbroeglin@gmail.com>
2022-08-16 20:25:59 +0100

    Fetching query works

--- public/script.js
+++ public/script.js
@@ -1,17 +1,20 @@
 const foodSearch = document.getElementById('food-search');
-const btn = document.getElementById('btn');
+const searchButton = document.getElementById('btn');
 const cardsDisplay = document.getElementById('cards-display');
 
-const options = {
-	method: 'GET',
-	headers: {
-        //to be found in doc
-		'xxx': 'SIGN-UP-FOR-KEY',
-		'xxx': 'edamam-recipe-search.p.rapidapi.com'
-	}
-};
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+//         //to be found in doc
+// 		'xxx': 'SIGN-UP-FOR-KEY',
+// 		'xxx': 'edamam-recipe-search.p.rapidapi.com'
+// 	}
+// };
 
-fetch('https://api.edamam.com/api/recipes/v2/', options)
+const app_id = '1ab55c64';
+const api_key = 'c86872049aff2debad57830e690d77c8'
+
+fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=${app_id}&app_key=${api_key}`)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
