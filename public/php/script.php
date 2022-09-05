76a94a08d1388a5f0f99a21cfe03278231c3aa81
AxelBroeglin <axelbroeglin@gmail.com>
2022-09-05 09:55:45 +0100

    App can retrieve info from script.php

--- public/php/script.php
+++ public/php/script.php
@@ -1,28 +1,29 @@
 <?php
-include 'connection.php';
+// include 'connection.php';
 
-//Connect to database
-$server = mysql_connect($host, $username, $password);
-$connection = mysql_select_db($database, $server);
+// //Connect to database
+// $server = mysql_connect($host, $username, $password);
+// $connection = mysql_select_db($database, $server);
 
-//Perform query
-$myquery = "
-  SELECT * FROM  `users`
-  ";
-$query = mysql_query($myquery);
+// //Perform query
+// $myquery = "
+//   SELECT * FROM  `users`
+//   ";
+// $query = mysql_query($myquery);
 
-$data = 'dsddsdsds';
+// $data = 'dsddsdsds';
 
-//Error handling
-if ( ! $query ) {
-    echo mysql_error();
-    die;
-}
+// //Error handling
+// if ( ! $query ) {
+//     echo mysql_error();
+//     die;
+// }
 
-echo json_encode('query');
+// echo json_encode('query');
 
-// close connection
-mysql_close($server);
-
+// // close connection
+// mysql_close($server);
+$test = 'test test';
+echo json_encode($test);
 
 ?>
