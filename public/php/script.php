8984c7e63adccf35f4a2c0974762ab28034a0af8
Axel Broeglin <axelbroeglin@gmail.com>
2022-09-07 19:51:32 +0100

    Blank file, script not working

--- public/php/script.php
+++ public/php/script.php
@@ -1,40 +1,4 @@
 <?php
 include 'connection.php';
 
-
-try{
-  $dbh = new PDO("mysql:host=$host;dbname=$database", $user, $password);
-}catch(PDOException $e)
-{
-  echo json_encode("Error:" . $e->getMessage() . "<br/>");
-  die;
-}
-// try{
-//   $db = new PDO ($dsn, $username, $password);
-// } catch(PDOException $e)
-// {
-//   print "Error:" . $e->getMessage() . "<br/>";
-//   die;
-// }
-
-// $conn = new mysqli($host, $username, $password,$database);
-
-
-//Perform query
-// $myquery = "
-// SELECT name FROM users 
-//   ";
-// $query = mysqli_query($conn, $myquery);
-
-
-////Error handling
-// if ( ! $query ) {
-//     echo json_encode(mysql_error());
-//     die;
-// }
-
-// echo json_encode($query);
-
-// // close connection
-// mysqli_close($server);
-// ?>
+?>
