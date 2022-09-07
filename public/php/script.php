201acfdfab4b5aeca0cef361ae359a6bf703d00a
Axel Broeglin <axelbroeglin@gmail.com>
2022-09-07 21:37:36 +0100

    Work on db connection

--- public/php/script.php
+++ public/php/script.php
@@ -1,5 +1,18 @@
 <?php
-//include 'connection.php';
+print_r('test');
 
-print_r('succes');
+$username = "axelbrgln";  
+$password = "Lepanache35";
+$host = "ftp.cluster031.hosting.ovh.net";
+$database="axelbrgln";
+
+$dsn = 'mysql:host=ftp.cluster031.hosting.ovh.net;dbname=axelbrgln';
+print_r($dsn);
+try{
+    $pdo = new PDO($dsn, $username, $password);
+}catch(PDOException $ex){
+    echo $ex->getMessage()." la ligne est".$ex->getLine();
+}
+// include 'connection.php';
+
 ?>
