93f3279eb840896b0bef1178d33b88f49d55c49d
Axel Broeglin <axelbroeglin@gmail.com>
2022-09-07 15:40:15 +0100

    Update script.php

--- public/php/script.php
+++ public/php/script.php
@@ -2,26 +2,39 @@
 include 'connection.php';
 
 
-$conn = new mysqli($host, $username, $password,$database) or die("Connect failed: %s\n". $conn -> error);
-// //Connect to database
-// $server = mysqli_connect($host, $username, $password);
-// $connection = mysqli_select_db($database, $server);
+try{
+  $dbh = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+}catch(PDOException $e)
+{
+  echo json_encode("Error:" . $e->getMessage() . "<br/>");
+  die;
+}
+// try{
+//   $db = new PDO ($dsn, $username, $password);
+// } catch(PDOException $e)
+// {
+//   print "Error:" . $e->getMessage() . "<br/>";
+//   die;
+// }
+
+// $conn = new mysqli($host, $username, $password,$database);
+
 
 //Perform query
-$myquery = "
-SELECT `name` FROM `users` 
-  ";
-$query = mysqli_query($conn, $myquery);
+// $myquery = "
+// SELECT name FROM users 
+//   ";
+// $query = mysqli_query($conn, $myquery);
 
 
-//Error handling
-if ( ! $query ) {
-    echo json_encode(mysql_error());
-    die;
-}
+////Error handling
+// if ( ! $query ) {
+//     echo json_encode(mysql_error());
+//     die;
+// }
 
-echo json_encode($query);
+// echo json_encode($query);
 
-// close connection
-mysqli_close($server);
-?>
+// // close connection
+// mysqli_close($server);
+// ?>
