0d32cc880b4ff392d929b4d9b307b7c5e954d92d
Axel Broeglin <axelbroeglin@gmail.com>
2022-09-29 14:02:54 +0200

    Update login.inc.php

--- public/includes/login.inc.php
+++ public/includes/login.inc.php
@@ -1,1 +1,22 @@
+<?php
 
+if(isset($_POST["submit"])){
+
+    // //Grabbing the data
+    $uid = $_POST["uid"];
+    $password = $_POST["password"];
+
+    // //Instantiate LoginContr class
+
+    include "../classes/connection.classes.php";
+    include "../classes/login.classes.php";
+    include "../classes/login-contr.classes.php";
+    $login = new LoginContr($uid, $password);
+
+    // //Running error handlers and user login
+    $login->loginUser();
+
+    //Going back to front page
+    header("location: ../index.php?error=none");
+
+}   
