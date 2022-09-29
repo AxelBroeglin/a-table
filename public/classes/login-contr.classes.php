597f9dcf927da547d4870bfecf498ce03a305b72
Axel Broeglin <axelbroeglin@gmail.com>
2022-09-29 14:01:29 +0200

    Update login-contr.classes.php

--- public/classes/login-contr.classes.php
+++ public/classes/login-contr.classes.php
@@ -1,1 +1,63 @@
 
+<?php
+
+class Login extends Dbh {
+
+    protected function getUser($uid, $password){
+        $stmt = $this->connect()->prepare('SELECT users_password FROM Users WHERE users_uid = ? OR users_email = ?;');
+
+        if(!$stmt->execute(array($uid, $password))) {
+            $stmt = null;
+            header("location: ../index.php?error=stmtfailed");
+            exit();
+        }
+
+        if($stmt->rowCount() == 0){
+            $stmt = null;
+            header("location: ../index.php?error=usernotfound");
+            exit();
+        }
+
+        $passwordHashed = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $checkPassword = password_verify($password, $passwordHashed[0]["users_password"]);
+
+        if($checkPassword == false){
+            $stmt = null;
+            header("location: ../index.php?error=wrongpassword");
+            exit();
+        } elseif($check_password == true){
+            $stmt = $this->connect()->prepare('SELECT * FROM Users WHERE users_uid = ? OR users_email = ? AND users_password = ?;');
+
+            if(!$stmt->execute(array($uid, $uid, $password))) {
+                $stmt = null;
+                header("location: ../index.php?error=stmtfailed");
+                exit();
+            }
+
+            if($stmt->rowCount() == 0){
+                $stmt = null;
+                header("location: ../index.php?error=usernotfound");
+                exit();
+            }
+
+            $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            session_start();
+            $_SESSION["userid"] = $user[0]["users_id"];
+            $_SESSION["useruid"] = $user[0]["users_uid"];
+
+            $stmt = null;
+
+
+
+
+            // $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            // session_start();
+            // $_SESSION["userid"] = $user[0]["users_id"];
+            // $_SESSION["useruid"] = $user[0]["users_uid"];
+
+            // $stmt = null;
+        }
+    }
+}
