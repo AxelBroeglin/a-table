e435953268b3970e5f0baefc08fec22b7ec25a6a
Axel Broeglin <axelbroeglin@gmail.com>
2022-10-20 14:30:10 +0100

    Create add-to-calendar-contr.classes.php

---
+++ public/includes/add-to-calendar-contr.classes.php
@@ -0,0 +1,34 @@
+<?php
+
+class loginContr extends Login {
+
+    private $uid;
+    private $password;
+    
+    public function __construct($uid, $password) {
+        $this->uid = $uid;
+        $this->password = $password;
+    }
+
+    public function loginUser() {
+        if($this->emptyInput() == false) {
+            // echo "Empty input!";
+            header("location: ../index.php?error=emptyinput");
+            exit();
+        }
+
+        $this->getUser($this->uid, $this->password);
+    }
+
+    private function emptyInput() {
+        $result;
+        if(empty($this->uid) || empty($this->password)) {
+            $result = false;
+        }
+        else {
+            $result = true;
+        }
+        return $result;
+    }
+
+}
