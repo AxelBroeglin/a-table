5d0dfb253746a0555d415bcab80b4de6c85165a2
Axel Broeglin <axelbroeglin@gmail.com>
2023-03-31 17:49:37 +0100

    Add files via upload

--- public/includes/add-to-calendar.inc.php
+++ public/includes/add-to-calendar.inc.php
@@ -4,26 +4,51 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 if(isset($_GET["date"]) && isset($_GET["title"]) && isset($_GET["url"]) && isset($_GET["type"])){
+    include "../classes/connection.classes.php";
+    $dbh = new Dbh();
+    $connection = $dbh->getConnection();
+
+
     //Grabbing the data
-    $userid = $_SESSION["useruid"];
+    $userid = $_SESSION["userid"];
     $date = $_GET["date"];
     $title = $_GET["title"];
     $url = $_GET["url"];
     $type = $_GET["type"];
+    $searchurl = $_GET["searchurl"];
 
+    $column = "";
+    if($type == "lunch") {
+        $column = "lunch";
+    } else if($type == "dinner") {
+        $column = "dinner";
+    }
 
+    // First, check if a row already exists in the Meals table for the given user and date
+    $check_query = "SELECT * FROM Meals WHERE users_id = '$userid' AND date = '$date'";
+    $result = $connection->query($check_query);
 
-    //Instantiate LoginContr class
+    if (!$result) {
+        // query failed
+    } else {
+        if ($result->rowCount() > 0) {
+            // a row already exists, so update it
+            $update_query = "UPDATE Meals SET ".$column."_name = '$title', ".$column."_url = '$url' WHERE users_id = '$userid' AND date = '$date'";
+            $result = $connection->query($update_query);
+        } else {
+            // a row does not exist, so insert a new one
+            $insert_query = "INSERT INTO Meals (users_id, date, $column"."_name, $column"."_url) VALUES ('$userid', '$date', '$title', '$url')";
+            $result = $connection->query($insert_query);
+        }
+    }
 
-    include "../classes/connection.classes.php";
-    include "../classes/add-to-calendar.classes.php";
-    include "../classes/add-to-calendar-contr.classes.php";
-
-
-    //Going back to front page
-    header("location: ../index.php?error=none");
+    if (!$result) {
+        // query failed
+    } else {
+        //Going back to front page
+        header("location: $searchurl");
+    }
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
